785b3f19edb8ae9c4b10ff9807dd5d363764aef5
ARYANDHIRHI004 <aryandhirhi004@gmail.com>
2026-07-29 19:55:38 +0530

    fix: fix shared ui package import bug.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@workspace/ui/components/button"
+import {Button } from "@workspace/ui/components/button"
 
 export default function Page() {
   return (
     <div className="bg-amber-400">
-
+      <Button>
+        aryan
+      </Button>
     </div>
   )
 }
