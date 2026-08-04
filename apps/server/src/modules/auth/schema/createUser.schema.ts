a85f71e2b9c6ba071b0070eb6b5c01f102396fa9
ARYANDHIRHI004 <aryandhirhi004@gmail.com>
2026-08-04 23:52:19 +0530

    feat: setup drizzle and better auth

---
+++ apps/server/src/modules/auth/schema/createUser.schema.ts
@@ -0,0 +1,8 @@
+import z from "zod";
+
+export const createUserSchema = z.object({
+    name: z.string().nonempty(),
+    email: z.string().email().nonempty(),
+    password: z.string().nonempty(),
+    username: z.string().nonempty(),
+})
---
+++ packages/db/src/db.ts
@@ -0,0 +1,8 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+export function createDb(connectionString: string) {
+  const client = postgres(connectionString);
+
+  return drizzle(client);
+}
